faf1fb263d527527e268ec40310b30168de3ea79
Asianzu Henry <asianzuhenry@gmail.com>
2026-05-13 18:20:27 +0300

    add page analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <AppRoutes />
     </BrowserRouter>
   );
